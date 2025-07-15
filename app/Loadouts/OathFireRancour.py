6355b64faf7c762c2f5c4e81154ac58a1110b2a2
jayrhaft <31804723+jayrhaft@users.noreply.github.com>
2025-07-15 19:35:18 -0400

    Create OathFireRancour.py
    Full Oathplate, Rancour, Firecapers...AKA Stud

---
+++ app/Loadouts/OathFireRancour.py
@@ -0,0 +1,25 @@
+from Player import Player
+
+
+stats = {
+    'hp_level': 99,
+    'attack_level': 99,
+    'strength_level': 99,
+    'def_level': 99,
+    'magic_level': 99,
+    'ranged_level': 99,
+    'prayer_level': 99,
+
+    'slash_attack_bonus': 82,
+    'stab_attack_bonus': 44,
+    'crush_attack_bonus': 44,
+    'magic_attack_bonus': 0,
+    'ranged_attack_bonus': 0,
+
+    'melee_strength_bonus': 59,
+    'magic_strength_bonus': 0,
+    'ranged_strength_bonus': 0,
+}
+
+
+player = Player(stats=stats)
